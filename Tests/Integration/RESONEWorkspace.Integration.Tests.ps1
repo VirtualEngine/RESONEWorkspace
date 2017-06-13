f795b81ce8ad8b209776262038458deedf986746
Iain Brighton <iainbrighton@hotmail.com>
2017-06-13 11:38:59 +0100

    Adds additional integration test coverage

--- Tests/Integration/RESONEWorkspace.Integration.Tests.ps1
+++ Tests/Integration/RESONEWorkspace.Integration.Tests.ps1
@@ -8,4 +8,19 @@ Describe 'Integration\RESONEWorkspace' {
         { Import-Module (Join-Path -Path $RepoRoot -ChildPath "$moduleName.psd1") -Force } | Should Not Throw;
 
     }
-}
+
+    $dscResourcesPath = Get-ChildItem -Path (Resolve-Path "$PSScriptRoot\..\..\DSCResources").Path -Directory -Exclude '_*';
+    foreach ($module in $dscResourcesPath) {
+
+        ## Cannot load composite resources..
+        $moduleManifestPath = Join-Path -Path $module.FullName -ChildPath "$($module.Name).psm1";
+        if (Test-Path -Path $moduleManifestPath) {
+
+            It "Should load resource '$($module.Name)' without throwing" {
+                { Import-Module $moduleManifestPath -Force } | Should Not Throw;
+            }
+        
+        }
+    } #end foreach module
+
+} #end describe
